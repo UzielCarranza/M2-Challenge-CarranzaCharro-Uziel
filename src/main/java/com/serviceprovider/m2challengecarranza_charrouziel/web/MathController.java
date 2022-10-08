e17fe3f8ccca5e1e27ec41a08eb443d4e6191d0b
UzielCarranza <uzielcarranzac97@gmail.com>
2022-10-08 11:14:09 -0400

    created controller that handles math operation requests

--- src/main/java/com/serviceprovider/m2challengecarranza_charrouziel/web/MathController.java
+++ src/main/java/com/serviceprovider/m2challengecarranza_charrouziel/web/MathController.java
@@ -1,10 +1,23 @@
 package com.serviceprovider.m2challengecarranza_charrouziel.web;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.serviceprovider.m2challengecarranza_charrouziel.data.MathSolution;
+import com.serviceprovider.m2challengecarranza_charrouziel.dto.MathSolutionDTO;
+import com.serviceprovider.m2challengecarranza_charrouziel.service.MathService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MathController {
 
+    private MathService mathService = new MathService();
 
+    public MathController(MathService mathService) {
+        this.mathService = mathService;
+    }
 
+    @PostMapping("/add")
+    @ResponseStatus(HttpStatus.CREATED)
+    public MathSolution doMathOperation(@RequestBody MathSolutionDTO mathSolutionDTOAdd) {
+        return mathService.addition(mathSolutionDTOAdd);
+    }
 }
