c4ff2152944ac4a531ae99c4ca3fb21c358168b2
UzielCarranza <uzielcarranzac97@gmail.com>
2022-10-08 11:12:49 -0400

    created Math dto to request a specific json object

--- src/main/java/com/serviceprovider/m2challengecarranza_charrouziel/dto/MathSolutionDTO.java
+++ src/main/java/com/serviceprovider/m2challengecarranza_charrouziel/dto/MathSolutionDTO.java
@@ -1,2 +1,48 @@
-package com.serviceprovider.m2challengecarranza_charrouziel.dto;public class MathSolutionDTO {
+package com.serviceprovider.m2challengecarranza_charrouziel.dto;
+
+public class MathSolutionDTO {
+
+    //    FIELDS
+    private int operand1;
+    private int operand2;
+
+    public MathSolutionDTO() {
+    }
+
+    public MathSolutionDTO(int operand1, int operand2) {
+        this.operand1 = operand1;
+        this.operand2 = operand2;
+    }
+
+    public int add() {
+        return operand1 + operand2;
+    }
+
+    public int subtract() {
+        return operand1 - operand2;
+    }
+
+    public int multiply() {
+        return operand1 * operand2;
+    }
+
+    public int divide() {
+        return operand1 / operand2;
+    }
+    public int getOperand1() {
+        return operand1;
+    }
+
+    public void setOperand1(int operand1) {
+        this.operand1 = operand1;
+    }
+
+    public int getOperand2() {
+        return operand2;
+    }
+
+    public void setOperand2(int operand2) {
+        this.operand2 = operand2;
+    }
+
 }
