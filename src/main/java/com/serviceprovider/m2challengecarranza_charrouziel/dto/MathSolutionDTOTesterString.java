3a085c9b2e3c1d7e9f6da9e7f7a745a39c2c28f9
UzielCarranza <uzielcarranzac97@gmail.com>
2022-10-08 17:47:39 -0400

    Added tester dto to test for string values

--- src/main/java/com/serviceprovider/m2challengecarranza_charrouziel/dto/MathSolutionDTOTesterString.java
+++ src/main/java/com/serviceprovider/m2challengecarranza_charrouziel/dto/MathSolutionDTOTesterString.java
@@ -1,2 +1,31 @@
-package com.serviceprovider.m2challengecarranza_charrouziel.dto;public class MathSolutionDTOTesterString {
+package com.serviceprovider.m2challengecarranza_charrouziel.dto;
+
+public class MathSolutionDTOTesterString {
+    //    FIELDS
+    private int operand1;
+    private String operand2;
+
+    public MathSolutionDTOTesterString() {
+    }
+
+    public MathSolutionDTOTesterString(int operand1, String operand2) {
+        this.operand1 = operand1;
+        this.operand2 = operand2;
+    }
+
+    public int getOperand1() {
+        return operand1;
+    }
+
+    public void setOperand1(int operand1) {
+        this.operand1 = operand1;
+    }
+
+    public String getOperand2() {
+        return operand2;
+    }
+
+    public void setOperand2(String operand2) {
+        this.operand2 = operand2;
+    }
 }
