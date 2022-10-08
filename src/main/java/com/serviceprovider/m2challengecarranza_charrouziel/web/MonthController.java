e8b916290223cc1b93fa052c18b5915ca31325c6
UzielCarranza <uzielcarranzac97@gmail.com>
2022-10-08 16:49:55 -0400

    challenge submission

--- src/main/java/com/serviceprovider/m2challengecarranza_charrouziel/web/MonthController.java
+++ src/main/java/com/serviceprovider/m2challengecarranza_charrouziel/web/MonthController.java
@@ -2,29 +2,45 @@ package com.serviceprovider.m2challengecarranza_charrouziel.web;
 
 
 import com.serviceprovider.m2challengecarranza_charrouziel.data.Month;
-import com.serviceprovider.m2challengecarranza_charrouziel.service.MonthService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 @RestController
 public class MonthController {
 
     //    Fields
-    private final MonthService monthService;
     private int min = 1;
     private int max = 12;
     private int random_int;
-
-    public MonthController(MonthService monthService) {
-        this.monthService = monthService;
-    }
+    //    Data
+    private List<Month> monthList = new ArrayList<>(Arrays.asList(
+            new Month(1, "January"),
+            new Month(2, "February"),
+            new Month(3, "March"),
+            new Month(4, "April"),
+            new Month(5, "May"),
+            new Month(6, "June"),
+            new Month(7, "July"),
+            new Month(8, "August"),
+            new Month(9, "September"),
+            new Month(10, "October"),
+            new Month(11, "November"),
+            new Month(12, "December")
+    ));
 
     //    GET METHODS
     @GetMapping("/month/{monthNumber}")
     @ResponseStatus(HttpStatus.OK)
     public Month getMonthByNumber(@PathVariable int monthNumber) {
-        return monthService.getMonthByNumberService(monthNumber);
+        if (monthNumber < 1 || monthNumber > 12) {
+            throw new IllegalArgumentException("Input must be between 1 and 12");
+        }
+        return monthList.stream().filter(month -> month.getNumber() == monthNumber).findFirst().orElse(null);
     }
 
     //    get a random month
@@ -33,7 +49,7 @@ public class MonthController {
     public Month getRandomMonth() {
 //        https://www.educative.io/answers/how-to-generate-random-numbers-in-java
         random_int = (int) Math.floor(Math.random() * (max - min + 1) + min);
-        return monthService.getMonthByNumberService(random_int);
+        return getMonthByNumber(random_int);
     }
 
 }
