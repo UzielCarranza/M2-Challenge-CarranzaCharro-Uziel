406338e690c0290a99c697f989f9dbbc75e3b0ef
UzielCarranza <uzielcarranzac97@gmail.com>
2022-10-08 11:13:17 -0400

    created math solution class

--- src/main/java/com/serviceprovider/m2challengecarranza_charrouziel/data/MathSolution.java
+++ src/main/java/com/serviceprovider/m2challengecarranza_charrouziel/data/MathSolution.java
@@ -7,24 +7,40 @@ import java.util.Objects;
 
 public class MathSolution {
 
-//    FIELDS
+    //    FIELDS
     private int operand1;
     private int operand2;
-    private List<String> operation = new ArrayList<>(Arrays.asList("add", "subtract", "multiply", "divide"));
-    private String answer;
+    private String operation;
+    private int answer;
 
-//    CONSTRUCTOR
-    public MathSolution() {
-    }
+    //    CONSTRUCTOR
 
-    public MathSolution(int operand1, int operand2, List<String> operation, String answer) {
+
+    public MathSolution(int operand1, int operand2, String operation, int answer) {
         this.operand1 = operand1;
         this.operand2 = operand2;
         this.operation = operation;
         this.answer = answer;
     }
 
-//    GETTERS AND SETTERS
+
+
+    public int getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(int answer) {
+        this.answer = answer;
+    }
+
+    public String getOperation() {
+        return operation;
+    }
+
+    public void setOperation(String operation) {
+        this.operation = operation;
+    }
+
     public int getOperand1() {
         return operand1;
     }
@@ -41,43 +57,4 @@ public class MathSolution {
         this.operand2 = operand2;
     }
 
-    public List<String> getOperation() {
-        return operation;
-    }
-
-    public void setOperation(List<String> operation) {
-        this.operation = operation;
-    }
-
-    public String getAnswer() {
-        return answer;
-    }
-
-    public void setAnswer(String answer) {
-        this.answer = answer;
-    }
-
-//    OVERRIDE METHODS
-    @Override
-    public String toString() {
-        return "MathSolution{" +
-                "operand1=" + operand1 +
-                ", operand2=" + operand2 +
-                ", operation=" + operation +
-                ", answer='" + answer + '\'' +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        MathSolution that = (MathSolution) o;
-        return operand1 == that.operand1 && operand2 == that.operand2 && Objects.equals(operation, that.operation) && Objects.equals(answer, that.answer);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(operand1, operand2, operation, answer);
-    }
 }
