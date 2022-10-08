326f9e8377f31f1dafff1a433b2c73db611998d2
UzielCarranza <uzielcarranzac97@gmail.com>
2022-10-07 21:54:44 -0400

    added 3 test cases that check for exeption responses, successfull responses, and expected object body response

---
+++ src/test/java/com/serviceprovider/m2challengecarranza_charrouziel/web/MonthControllerTest.java
@@ -0,0 +1,69 @@
+package com.serviceprovider.m2challengecarranza_charrouziel.web;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.serviceprovider.m2challengecarranza_charrouziel.data.Month;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(MonthController.class)
+public class MonthControllerTest {
+
+    //TEST FOR MONTH CONTROLLER
+    @Autowired
+    private MockMvc mockMvc;
+
+    private ObjectMapper mapper = new ObjectMapper();
+
+
+    //    ENDPOINT GET MONTH BY NUMBER
+    //     @GetMapping("/month/{monthNumber}")
+
+    @Test
+    public void shouldReturnMonthBasedOnNumberIdentifier() throws Exception {
+//        Arrange
+        Month expectedMonth = new Month(3, "March");
+
+        String outputJson = mapper.writeValueAsString(expectedMonth);
+        int inputMonth = 3;
+
+        mockMvc.perform(get("/month/{monthNumber}", inputMonth)       // Act
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().json(outputJson));
+    }
+
+    @Test
+    public void shouldGet200HttpResponseOnGetByMonthByNumber() throws Exception {
+        // Arrange
+        int inputMonth = 1;
+        mockMvc.perform(get("/month/{monthNumber}", inputMonth)       // Act
+                )
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void shouldReturn422StatusCodeIfNumberIsOutOfBounds() throws Exception {
+        // Arrange
+        int inputMonth = 13;
+        mockMvc.perform(get("/month/{monthNumber}", inputMonth)       // Act
+                )
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity());
+    }
+}
