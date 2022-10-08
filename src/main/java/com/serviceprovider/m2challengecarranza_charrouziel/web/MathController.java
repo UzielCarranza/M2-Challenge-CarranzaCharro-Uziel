b1193ad5cd2a680bd53538b5d83e77bdfdbe9040
UzielCarranza <uzielcarranzac97@gmail.com>
2022-10-08 09:52:46 -0400

    added math controller to handle http requests regarding math operations

---
+++ src/main/java/com/serviceprovider/m2challengecarranza_charrouziel/web/MathController.java
@@ -0,0 +1,10 @@
+package com.serviceprovider.m2challengecarranza_charrouziel.web;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MathController {
+
+
+
+}
